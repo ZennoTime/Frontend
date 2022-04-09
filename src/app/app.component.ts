24da32756b142cbe903408dedfa85cdb347930a0
ZennoTime <99843517+ZennoTime@users.noreply.github.com>
2022-04-10 00:40:32 +0300

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { ItemComponent } from './item/item.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'todo-list';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'Делать дз по SQL', done: true },
+    { description: 'Делать дз по WebApi', done: false },
+    { description: 'Делать ОП', done: false },
+    { description: 'Делать Web', done: false },
+    { description: 'Постараться поспать', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  remove(item : any) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
 }
